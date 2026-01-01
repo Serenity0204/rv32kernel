7b1f86c807fbb7b3f36f9705f0b86ca7deed4ac9
Serenity0204 <yuhenglin0204@gmail.com>
2026-01-01 11:55:59 -0800

    added strlen

---
+++ lib/stdlib.c
@@ -0,0 +1,8 @@
+#include "stdlib.h"
+
+unsigned int strlen(const char* str)
+{
+    unsigned int len = 0;
+    while (str[len] != '\0') len++;
+    return len;
+}
---
+++ lib/stdlib.h
@@ -0,0 +1,6 @@
+#ifndef STDLIB_H
+#define STDLIB_H
+
+unsigned int strlen(const char* str);
+
+#endif
