a966a468d4d244a68a35f7f2fcb30b072b14529d
Serenity0204 <yuhenglin0204@gmail.com>
2026-01-02 22:49:24 -0800

    updated linker addr

--- core/common/Common.hpp
+++ core/common/Common.hpp
@@ -2,14 +2,19 @@
 
 #include <cstdint>
 #include <iostream>
+#include <unordered_map>
 
 // riscv32i
 using Byte = uint8_t;
 using Half = uint16_t;
 using Word = uint32_t;
 using Addr = uint32_t;
+using PageTable = std::unordered_map<Addr, Addr>;
 
 // Memory
-const Addr MEMORY_BASE = 0x80000000;
+const Addr MEMORY_BASE = 0x00000000;
 // 128MB
 const Addr MEMORY_SIZE = 1024 * 1024 * 128;
+
+// 1kb
+const Addr PAGE_SIZE = 4096;
